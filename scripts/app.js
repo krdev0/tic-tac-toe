3140517ce5c4aedec15fe6d9837e227820f79401
Kristaps Ritins <kritinsdev@gmail.com>
2021-11-17 13:30:35 +0200

    save player name function

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+let editedPlayer = 0;
+const players = [
+  {
+    name: "",
+    symbol: "X",
+  },
+  {
+    name: "",
+    symbol: "O",
+  },
+];
+
+const backdrop = document.querySelector("#backdrop");
+const playerConfigModal = document.querySelector("#config-overlay");
+
+const editPlayer1Btn = document.querySelector("#edit-player-1");
+const editPlayer2Btn = document.querySelector("#edit-player-2");
+const cancelPlayerModal = document.querySelector("#cancel-player-config");
+
+const configForm = document.querySelector("form");
+const configError = document.querySelector("#config-error");
+
+editPlayer1Btn.addEventListener("click", openPlayerConfig);
+editPlayer2Btn.addEventListener("click", openPlayerConfig);
+
+cancelPlayerModal.addEventListener("click", closePlayerConfig);
+backdrop.addEventListener("click", closePlayerConfig);
+
+configForm.addEventListener("submit", savePlayerName);
---
+++ scripts/config.js
@@ -0,0 +1,35 @@
+function openPlayerConfig(event) {
+  editedPlayer = Number(event.target.dataset.player);
+  playerConfigModal.style.display = "block";
+  backdrop.style.display = "block";
+}
+
+function closePlayerConfig() {
+  playerConfigModal.style.display = "none";
+  backdrop.style.display = "none";
+  configForm.firstElementChild.classList.remove("error");
+  configError.textContent = "";
+}
+
+function savePlayerName(event) {
+  event.preventDefault();
+  const formData = new FormData(event.target);
+  const enteredPlayerName = formData.get("playername").trim();
+
+  if (!enteredPlayerName) {
+    event.target.firstElementChild.classList.add("error");
+    configError.textContent = "Please enter a valid name";
+    return;
+  }
+
+  const updatedPlayerData = document.querySelector(
+    "#player-" + editedPlayer + "-data"
+  );
+
+  updatedPlayerData.children[1].textContent = enteredPlayerName;
+
+  players[editedPlayer - 1].name = enteredPlayerName;
+
+  playerConfigModal.style.display = "none";
+  backdrop.style.display = "none";
+}
